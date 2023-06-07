50962ead6a6d267a69d345d628fb7732fbfa1719
Brunella D'Anzi <75045014+bdanzi@users.noreply.github.com>
2023-06-07 10:13:31 +0200

    Add files via upload

---
+++ plugins/RecHitAnalyzer/RecHitAnalyzer.cc
@@ -0,0 +1,277 @@
+// -*- C++ -*-
+//
+// Package:    RecHits/RecHitAnalyzer
+// Class:      RecHitAnalyzer
+//
+//
+// Original Author:  Brunella D'Anzi
+//         Created:  Mon, 28 May 2023 17:16:39 GMT
+//
+//
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "DataFormats/Common/interface/DetSetVector.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "DataFormats/DetId/interface/DetId.h"
+#include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
+#include "DataFormats/SiStripCluster/interface/SiStripClusterCollection.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2D.h"
+#include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2DCollection.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
+#include "Geometry/TrackerGeometryBuilder/interface/TrackerGeometry.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+//#include "DataFormats/Common/interface/RangeSet.h"
+#include "TH2F.h"
+#include "TH3F.h"
+#include "TGraphErrors.h"
+#include "TStyle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "TFile.h"
+#include "TCanvas.h"
+#include "TPaveStats.h"
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<>
+// This will improve performance in multithreaded jobs.
+
+using reco::TrackCollection;
+
+class RecHitAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources> {
+public:
+  explicit RecHitAnalyzer(const edm::ParameterSet&);
+  ~RecHitAnalyzer() override;
+
+  static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+private:
+  void beginJob() override;
+  void analyze(const edm::Event&, const edm::EventSetup&) override;
+  void plotClusterShape(const SiStripCluster& cluster, const int sequentialId);
+  void plotRecHitPosition(const float x, const float y);
+  void plotRecHitErrors(const float x, const float y, const float xErr, const float yErr);
+  void plotDetIdMapping( std::map<uint32_t, int> detIdToSequentialNumber_);
+  void endJob() override;
+
+  // ----------member data ---------------------------
+  edm::EDGetTokenT<edmNew::DetSetVector<SiStripCluster>> clusterToken_;
+  edm::EDGetTokenT<SiStripRecHit2DCollection> recHitToken_;
+  std::map<uint32_t, int> detIdToSequentialNumber_;
+  int sequentialNumber_;
+  TH2F* recHitPositionHist_;
+  TH3F* clusterShapeHist_;
+  TH2F* recHitErrorsHist_;
+  TH2F* detIdMappingHist_;
+  
+};
+
+
+//
+// constructors and destructor
+//
+RecHitAnalyzer::RecHitAnalyzer(const edm::ParameterSet& iConfig)
+  :clusterToken_(consumes<edmNew::DetSetVector<SiStripCluster>>(iConfig.getParameter<edm::InputTag>("recHits"))),
+   recHitToken_(consumes<SiStripRecHit2DCollection>(iConfig.getParameter<edm::InputTag>("recHitCollection"))){
+  //now do what ever initialization is needed
+  sequentialNumber_ = 0;
+  recHitPositionHist_ = nullptr;
+  clusterShapeHist_ = nullptr;
+  recHitErrorsHist_ = nullptr;
+  detIdMappingHist_ = nullptr;
+ 
+}
+
+RecHitAnalyzer::~RecHitAnalyzer() {
+  // do anything here that needs to be done at desctruction time
+  // (e.g. close files, deallocate resources etc.)
+  //
+  // please remove this method altogether if it would be left empty
+}
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void RecHitAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
+  using namespace edm;
+  edm::Handle<edmNew::DetSetVector<SiStripCluster>> clusterHandle;
+  iEvent.getByToken(clusterToken_, clusterHandle);
+  
+  edm::Handle<SiStripRecHit2DCollection> recHitHandle;
+  iEvent.getByToken(recHitToken_, recHitHandle);
+  // Loop over each individual SiStripRecHit2DCollection pair
+      for (const auto& detSetPair : *recHitHandle) {
+	/*	int  detId = detSetPair.id();
+	int sequentialId = 0;
+	if (detIdToSequentialNumber_.find(detId) == detIdToSequentialNumber_.end()) {
+	  sequentialId = sequentialNumber_;
+	  detIdToSequentialNumber_[detId] = sequentialId;
+	  sequentialNumber_++;
+	} else {
+	  sequentialId = detIdToSequentialNumber_[detId];
+	}
+	*/
+	const auto& detSet = detSetPair;
+    
+	for (const auto& recHit : detSet) {
+	// Access information from SiStripRecHit2D                                                                     
+	float x = recHit.localPosition().x();
+	float y = recHit.localPosition().y();
+	float xErr = recHit.localPositionError().xx();
+	float yErr = recHit.localPositionError().yy();
+	auto detId_geographicalId = recHit.geographicalId();
+	int detId = detId_geographicalId.rawId();
+	int sequentialId = 0;                                                                                                                                                                          
+        if (detIdToSequentialNumber_.find(detId) == detIdToSequentialNumber_.end()) {                                                                                                                  
+          sequentialId = sequentialNumber_;                                                                                                                                                            
+          detIdToSequentialNumber_[detId] = sequentialId;                                                                                                                                              
+          sequentialNumber_++;                                                                                                                                                                         
+        } else {                                                                                                                                                                                       
+          sequentialId = detIdToSequentialNumber_[detId];                                                                                                                                              
+        }              
+	std::cout << "DetId_hits: " << detId << std::endl;
+	std::cout << "Local position (x, y): " << x << ", " << y << std::endl;
+	std::cout << "Local position errors (x, y): " << xErr << ", " << yErr << std::endl;
+	// Plot the recHit position, cluster shape, recHit errors, and DetId mapping                                                                                                                     
+	plotRecHitPosition(x, y);
+	plotClusterShape(*recHit.cluster(),sequentialId);
+	plotRecHitErrors(x, y, xErr, yErr);
+	// Plot the DetId mapping                                                                                                                                                                       
+	plotDetIdMapping(detIdToSequentialNumber_);
+      }
+    }
+}
+
+
+
+void RecHitAnalyzer::plotClusterShape(const SiStripCluster& cluster, const int sequentialId) {
+  // Plot the cluster shape (3D plot)
+      auto  amplitudes = cluster.amplitudes();
+      const int clusterSize = amplitudes.size();
+      const int clusterCharge = cluster.charge();
+      for (size_t i = 0; i < amplitudes.size(); ++i) {
+	//std::cout << "Strip Number " << i << std::endl;
+	clusterShapeHist_->Fill(clusterCharge, amplitudes[i], clusterSize);
+      }
+    }
+    
+    void RecHitAnalyzer::plotRecHitPosition(const float x, const float y) {
+      
+      // Plot the recHit position (2D plot)
+      recHitPositionHist_->Fill(x, y);
+      
+    }
+    
+    void RecHitAnalyzer::plotRecHitErrors(const float x, const float y, const float xErr, const float yErr) {
+      
+      // Plot the recHit errors (2D plot)
+      recHitErrorsHist_->Fill(xErr, yErr);
+    
+    }
+
+    void RecHitAnalyzer::plotDetIdMapping(std::map<uint32_t, int> detIdToSequentialNumber_) {
+      // Plot the DetId mapping (2D plot)
+      for (const auto& pair : detIdToSequentialNumber_) {
+	detIdMappingHist_->Fill(pair.second, pair.first);
+      }
+      
+    }
+    // ------------ method called once each job just before starting event loop  ------------
+    void RecHitAnalyzer::beginJob() {
+      
+      detIdMappingHist_ = new TH2F("detIdMappingHist", "DetId Mapping", sequentialNumber_, 0, sequentialNumber_, 1000, 0, 5);
+      detIdMappingHist_->GetXaxis()->SetTitle("Sequential Number");
+      detIdMappingHist_->GetYaxis()->SetTitle("DetId");
+
+      recHitErrorsHist_ = new TH2F("recHitErrorsHist", "RecHit Errors", 100, -0.001, 0.2, 500, -0.5, 30.0);
+      recHitErrorsHist_->GetXaxis()->SetTitle("x (mm)");
+      recHitErrorsHist_->GetYaxis()->SetTitle("y (mm)");
+      
+      recHitPositionHist_ = new TH2F("recHitPositionHist", "RecHit Position", 100, -5.5, 5.5, 100, -1.0, 1.0);
+      recHitPositionHist_->GetXaxis()->SetTitle("x (mm)");
+      recHitPositionHist_->GetYaxis()->SetTitle("y (mm)");
+
+      clusterShapeHist_ = new TH3F("clusterShapeHist", "Cluster Shape", 1500, 0, 1500, 256, 0, 256, 10, 0, 10);
+      clusterShapeHist_->GetXaxis()->SetTitle("Cluster Charge  (ehp)");
+      clusterShapeHist_->GetYaxis()->SetTitle("ADC Counts");
+      clusterShapeHist_->GetZaxis()->SetTitle("Cluster Size");
+      clusterShapeHist_->GetXaxis()->SetTitleOffset(1.5); // Adjust the offset for the X-axis label
+      clusterShapeHist_->GetYaxis()->SetTitleOffset(1.5); // Adjust the offset for the Y-axis label
+      clusterShapeHist_->GetZaxis()->SetTitleOffset(1.5); // Adjust the offset for the Z-axis label 
+
+    }
+    
+    // ------------ method called once each job just after ending the event loop  ------------
+    void RecHitAnalyzer::endJob() {
+      TCanvas* canvas_recHitPosition = new TCanvas("canvas_recHitPosition", "RecHit Position", 800, 600);
+      TCanvas* canvas_detIdMapping = new TCanvas("canvas_detIdMapping", "DetId Mapping", 1000, 600);
+      TCanvas* canvas_recHitErrors = new TCanvas("canvas_recHitErrors", "RecHit Errors", 800, 600);
+      TCanvas* canvas_clusterShapeHist = new TCanvas("canvas_clusterShapeHist", "Cluster Shape", 1000, 600);
+      canvas_recHitPosition->cd();
+      // Draw the histogram on the canvas                                                                                                                                                              
+      recHitPositionHist_->Draw("hist");
+      // Save the canvas as a PNG image                                                                                                                                                                 
+      canvas_recHitPosition->SaveAs("recHitPosition.png");                                                                                                                                              
+      delete canvas_recHitPosition;
+      // Draw the histogram on the canvas                                                                                                                                                               
+      canvas_detIdMapping->cd();
+      detIdMappingHist_->Draw("hist");
+      // Save the canvas as a PNG image                                                                                                                                                              
+      canvas_detIdMapping->SaveAs("detIdMapping.png");
+      delete canvas_detIdMapping;                                                                                                                                                                 
+      // Save the canvas as a PNG image                                                                                                                                                                 
+      canvas_recHitErrors->cd();
+      recHitErrorsHist_->Draw("hist");
+      canvas_recHitErrors->SaveAs("recHitErrors.png");
+      delete canvas_recHitErrors;
+      // Save the canvas as a PNG image                                                                                                                                                                 
+      canvas_clusterShapeHist->cd();
+      
+      TPaveStats* stats = dynamic_cast<TPaveStats*>(clusterShapeHist_->GetListOfFunctions()->FindObject("stats"));
+      // Relocate the stats box
+      if (stats) {
+	stats->SetX1NDC(0.25); // Set the new X-coordinate of the lower edge
+	stats->SetX2NDC(0.55); // Set the new X-coordinate of the upper edge
+	stats->SetY1NDC(0.75); // Set the new Y-coordinate of the lower edge
+	stats->SetY2NDC(0.95); // Set the new Y-coordinate of the upper edge
+	stats->Draw();
+	canvas_clusterShapeHist->Update();
+	}
+      
+      clusterShapeHist_->Draw();
+      canvas_clusterShapeHist->SaveAs("clusterShapeHist.png"); 
+      delete canvas_clusterShapeHist;
+      TFile output("RecHitAnalyzer.root", "RECREATE");
+      recHitPositionHist_->Write();
+      clusterShapeHist_->Write();
+      recHitErrorsHist_->Write();
+      detIdMappingHist_->Write();
+      output.Close();
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void RecHitAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(RecHitAnalyzer);
---
+++ python/recHitAnalyzer_cfg.py
@@ -0,0 +1,39 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("RecHitAnalyzerTest")
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("TrackingTools/TransientTrack/TransientTrackBuilder_cfi")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+#-------------------------------------------------                                                                                                                                                         
+# CALIBRATION                                                                                                                                                                                              
+#-------------------------------------------------                                                                                                                                                         
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = "124X_dataRun3_v10"
+process.es_prefer_GlobalTag = cms.ESPrefer('PoolDBESSource','GlobalTag')
+
+process.source = cms.Source("PoolSource",
+    # Input file(s)
+    fileNames = cms.untracked.vstring("file:step3.root")
+)
+process.myConverter = cms.EDProducer("SiStripRecHitConverter",
+    ClusterProducer = cms.InputTag("siStripClusters"),
+    rphiRecHits = cms.string("rphiRecHit"),
+    stereoRecHits = cms.string("stereoRecHit"),
+    matchedRecHits = cms.string("matchedRecHit"),
+    doMatching = cms.bool(True)
+)
+
+process.RecHitAnalyzerHelp = cms.EDAnalyzer("RecHitAnalyzer",
+    recHits = cms.InputTag("siStripClusters"),
+#    recHitCollection = cms.InputTag("myConverter", "rphiRecHit")
+    recHitCollection = cms.InputTag("myConverter", "stereoRecHit")
+)
+
+process.p = cms.Path(process.myConverter * process.RecHitAnalyzerHelp)
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+
+process.options = cms.untracked.PSet(wantSummary = cms.untracked.bool(True))
